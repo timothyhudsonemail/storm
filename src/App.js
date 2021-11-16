5eb7b4abac48f110e83c124c4a6b57e624d3b371
timothyhudsonemail <60154902+timothyhudsonemail@users.noreply.github.com>
2021-11-16 16:36:21 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-
 import './App.css';
 import $ from 'jquery';
-
-
 
 function App() {
 
   function storm1() {
-
     friendlyfunction();
-
     if ($('#clue').css('backgroundColor') ==='rgb(0, 0, 0)'){
-      $('#clue').css('backgroundColor','yellow');
+      $('#clue').css('backgroundColor','white');
       document.getElementById('friendly').play();
       setTimeout(storm1, 200 )
     }
@@ -19,78 +14,49 @@ function App() {
       $('#clue').css('backgroundColor','black');
       setTimeout(storm2, Math.floor(Math.random() * 6666) )
     }
-
   }
 
   function storm2() {
-
-    
     if($('#clue').css('backgroundColor') ==='rgb(0, 0, 0)'){
-      $('#clue').css('backgroundColor','yellow');
+      $('#clue').css('backgroundColor','white');
       document.getElementById('evil').play();
       setTimeout(storm2, 200 )
     }
     else{
-
       $('#clue').css('backgroundColor','black');
-      
       setTimeout(storm3, Math.floor(Math.random() * 6666) )
     }
-
   }
 
   function storm3() {
-
-    
     if($('#clue').css('backgroundColor') ==='rgb(0, 0, 0)'){
-      $('#clue').css('backgroundColor','yellow');
+      $('#clue').css('backgroundColor','white');
       document.getElementById('psycho').play();
       setTimeout(storm3, 200 )
     }
     else{
-
-      $('#clue').css('backgroundColor','black');
-      
+      $('#clue').css('backgroundColor','black'); 
       setTimeout(storm4, Math.floor(Math.random() * 6666) )
     }
-
   }
 
   function storm4() {
-
-    
     if($('#clue').css('backgroundColor') ==='rgb(0, 0, 0)'){
-      $('#clue').css('backgroundColor','yellow');
+      $('#clue').css('backgroundColor','white');
       document.getElementById('emo').play();
       setTimeout(storm4, 200 )
     }
     else{
-
       $('#clue').css('backgroundColor','black');
-      
       setTimeout(storm1, Math.floor(Math.random() * 6666) )
     }
-
   }
-
-
-
 
   function friendlyfunction() {
-      
       var audio2 = document.getElementById("radiofriendly");
           audio2.play();
-          audio2.loop = true;   
-          
+          audio2.loop = true;    
   }
-
- 
-  
- 
-
-  
-
-
 
   return (
     <div className="App"> 
@@ -99,14 +65,8 @@ function App() {
         </button>
         <div style={{display:'flex', flexDirection:'row',justifyContent:'space-around'}}>
           <div id='clue' style={{backgroundColor:'black', width:'500px', height:'500px'}}>
-            
           </div>
-          
         </div>
-       
-        
-        
-
         <audio id='friendly' src="https://cdn.artlist.io/artlist-sfx-aac/578771_Texture_-_Thunder%2c_Clap%2c_Large-01_normal.aac" type="audio/mpeg" />
         <audio id='evil' src="https://cdn.artlist.io/artlist-sfx-aac/578771_Texture_-_Thunder%2c_Clap%2c_Large-01_normal.aac" type="audio/mpeg" />
         <audio id='psycho' src="https://cdn.artlist.io/artlist-sfx-aac/578771_Texture_-_Thunder%2c_Clap%2c_Large-01_normal.aac" type="audio/mpeg" />
